1e78d3a744b63432a24c402ce7372eac425bda87
Adebayo Olowofoyeku <adebayoolowofoyeku@gmail.com>
2022-07-02 15:37:50 +0100

    first screen

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, Image, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.image} source={require('./assets/logo.png')} />
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.buttons}>
+        <TouchableOpacity style={styles.signUp}>
+          <Text style={{color: '#fff', textAlign: 'center', fontSize: 24, fontWeight: '600'}}>Sign Up</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.logIn}>
+          <Text style={{color: '#000', textAlign: 'center', fontSize: 24, fontWeight: '600'}}>Log In</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +22,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+    marginHorizontal: 35 
+  },
+
+  image: {
+    position: 'absolute',
+    top: '35%',
+    alignSelf: 'center'
+  },
+
+  signUp: {
+    padding: 18,
+    backgroundColor: '#0079BE',
+    borderRadius: 40
+  },
+
+  logIn: {
+    padding: 18,
+    backgroundColor: '#F0F0F0',
+    borderRadius: 40,
+    marginTop: 15,
+    marginBottom: 20
   },
 });
